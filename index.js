c6876c6d01dc444f6525f912d6c8ab300ff584da
Lira11io <Lira11pm@mail.ru>
2023-03-25 22:39:57 +0300

    add files

---
+++ index.js
@@ -0,0 +1,43 @@
+//Ответ на 1 практическое задание
+function message() {
+  console.log("Я учу JavaScript!");
+}
+console.log(message());
+
+const showMessage = () => console.log("Я учу JavaScript!");
+showMessage();
+
+/* function sum(a, b) {
+  return a + b;
+}
+console.log(sum(2, 3)); */
+
+/* function sum(a, b) {
+  console.log(a + b);
+}
+sum(10, 2); */
+
+/* function sum(a, b) {
+  console.log(a + b);
+}
+let func = sum;
+func(10, 3); */
+
+/* const sum = (a, b) => a + b;
+console.log(sum(1, 2)); */
+
+/* function showX(x) {
+  return x;
+}
+console.log(showX(28));
+
+function sum(x, y = 0) {
+  return x + y;
+}
+console.log(sum(5));
+
+console.log(foo);
+
+let foo = "Cat";
+
+console.log(foo); */
